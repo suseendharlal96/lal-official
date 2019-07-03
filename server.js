4fa1b9da3e86eb5dbead9c842cebb38efcfacdde
suseendhar lal <suseendhar.l@object-frontier.com>
2019-07-04 01:17:31 +0530

    init person-vue

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 var express = require('express');
-var app = express();
-var port = process.env.port || 5000;
-app.use(express.static(__dirname));
-
-app.get('/', function(req, res) {
-  res.render('index');
-});
-
-app.listen(port, function() {
-  console.log('app running');
-});
+var path = require('path');
+var serveStatic = require('serve-static');
+app = express();
+app.use(serveStatic(__dirname));
+var port = process.env.PORT || 5000;
+app.listen(port);
+console.log('server started ' + port);
